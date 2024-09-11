f5ac94a851514c50784c860e8a5f70f39aeb56fa
Rodrigo da Silva Portes <rodrigo.portes@al.infnet.edu.br>
2024-09-11 09:19:47 -0300

    Adicionando exercicio02

--- src/exercises/Exercise01.jsx
+++ src/exercises/Exercise01.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import img from '../img/praia.jpg';
 import '../css/Exercise01.css'; 
+import Exercicio02 from "./Exercise02";
  
 
 const Exercise01 = () => {
   return (
     <div className="cartao">
-      <div className="container-img"></div>   
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore officiis quasi omnis neque dignissimos id saepe rem. Hic, dicta reprehenderit molestiae consequatur corrupti sequi totam aperiam mollitia voluptatum quod illum?</p>
+      <div className="container-img">
+        <img src={img} title="praia"/>
+      </div>  
+      <div className="container-texto">
+        <h1>Concorra a uma viagem grátis</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore officiis quasi omnis neque 
+        dignissimos id saepe rem. Hic, dicta reprehenderit molestiae consequatur corrupti sequi totam aperiam mollitia voluptatum quod illum?</p>
+        <Exercicio02/>
+
+      </div>
     </div>
   );
 };
--- src/exercises/Exercise02.jsx
+++ src/exercises/Exercise02.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Exercise02 = () => {
   return (
     <>
+      <button style={{backgroundColor: "whitesmoke", color: "rgb(86, 185, 224)", fontSize: "24px"}}>Saiba mais!</button>
     </>
   );
 };
